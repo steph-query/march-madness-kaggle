4ad4de7231c35accd01ec6333bedf9cfca17298f
Nick James <mrnickjames@hotmail.co.uk>
2016-02-28 21:58:46 -0800

    corrects plus_minus script and creates database table called team_data that includes plus minus.

--- team_plus_minus.py
+++ team_plus_minus.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sqlite3 as lite
+import pdb 
 
 conn = lite.connect('./data/database.sqlite')
 
@@ -7,16 +8,16 @@ regular_season_query = "SELECT * FROM RegularSeasonCompactResults;"
 teams_query = "SELECT * FROM Teams;"
 
 regular_season_df = pd.read_sql(regular_season_query, conn)
-regular_season_df = regular_season_df[regular_season_df['Year']]
+regular_season_df['diff'] = regular_season_df['Wscore'] - regular_season_df['Lscore']
 teams_df = pd.read_sql(teams_query, conn)
 conn.rollback()
 
 def calc_diffs(dataframe):
     diff_dict = {}
     for i, row in dataframe.iterrows():
-        winner = row.Wteam
-        loser = row.Lteam
-        diff = row.diff
+        winner = row['Wteam']
+        loser = row['Lteam']
+        diff = row['diff']
         if winner in diff_dict.keys():
             diff_dict[winner] += diff
         else:
@@ -24,14 +25,38 @@ def calc_diffs(dataframe):
         if loser in diff_dict.keys():
             diff_dict[loser] -= diff
         else:
-            diff_dict[loser = (-1 * diff)]
+            diff_dict[loser] = (0 - diff)
     return diff_dict
 
 def yearly_team_plus_minus(teams_df, games_df):
     years = [2012, 2013, 2014, 2015]
     for year in years:
-        games_year = games_df[games_df['Year'] == year]
-        teams_df['SeasonPlusMinus_{}'.format(str(year)] = teams['TeamId'].applymap(lambda x: calc_diffs(games_year)[x])
+        games_year = games_df[games_df['Season'] == year]
+        games_year_diffs = calc_diffs(games_year)
+        teams_df['SeasonPlusMinus_{}'.format(str(year))] = teams_df['Team_Id'].apply(lambda x: check_keys(x, games_year_diffs))
     return teams_df
 
+def check_keys(key, dict):
+    try:
+        return dict[key]
+    except:
+        return None
+
 teams_with_yearly_plus_minus = yearly_team_plus_minus(teams_df, regular_season_df)
+
+teams_with_yearly_plus_minus.to_sql('team_data', conn, if_exists='replace')
+
+
+# """
+# Data Structures: 
+
+# {"Duke": {"3-pointers-for": [8, 12, 4, 13, 9],
+#             "3-pointers-allowed": []
+
+#     }
+# }
+# Output table: 
+
+# 1 | Duke | +150 | 4.5 | 3.5 | 
+
+# """
